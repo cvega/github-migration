50e8f7b9ca699ec0b24de72d33b3f0652e4b77f8
Casey Vega <casey.vega@gmail.com>
2026-02-15 13:53:47 -0800

    fix: update GitHub status label from 'Operational' to 'Healthy'

--- src/lib/components/GitHubStatus.svelte
+++ src/lib/components/GitHubStatus.svelte
@@ -28,7 +28,7 @@
 			<span class="absolute inline-flex h-full w-full animate-ping rounded-full bg-green-400 opacity-75"></span>
 			<span class="relative inline-flex h-2 w-2 rounded-full bg-green-400"></span>
 		</span>
-		GitHub: Operational
+		GitHub: Healthy
 	</span>
 {:else}
 	<a
